dd5547e6b6d22edb5008974bbb8cc7622cfdbc1e
David Mbagwu <davidmbagwu@gmail.com>
2024-07-29 16:00:44 -0500

    Personalized user page for logged in users

--- nsbe_project/stage/urls.py
+++ nsbe_project/stage/urls.py
@@ -8,7 +8,7 @@ from django.urls import re_path
 from django.views.static import serve
 
 urlpatterns = [
-    path('', views.index, name='stage-index'),
+    path('home/', views.index, name='stage-index'),
     path('about/', views.about, name='stage-about'),
     path('signup/', views.signup, name='stage-signup'),
     path('directory/', views.directory, name='stage-directory'),
@@ -17,7 +17,7 @@ urlpatterns = [
     path('points/', views.points, name='stage-points'),
     path('profile/', views.profile, name='stage-profile'),
     path('gallery/', views.gallery, name='stage-gallery'),
-    path('login/', views.login_view, name='stage-login'),
+    path('', views.login_view, name='stage-login'),
     path('logout/', views.logout_view, name='stage-logout'),
     path('adminOnly/', views.adminOnly, name='stage-adminOnly'),
 ]
